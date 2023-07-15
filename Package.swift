2b5967b32a10faec0416714565822df0ec7856d4
tomieq <tomieq@wp.pl>
2023-07-15 16:04:02 +0200

    Initial Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "html.swift",
+    dependencies: [
+    ],
+    targets: [
+        .executableTarget(
+            name: "html.swift",
+            dependencies: [],
+            path: "Sources")
+    ]
+)
